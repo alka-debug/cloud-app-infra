9b88915a407d5be035f7c98efa5712866ff13e5f
alka-debug <kohlialka22@gmail.com>
2025-10-06 17:35:48 +0530

    app,docker,terraform

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+provider "docker" {}
+resource "docker_image" "flask_app" {
+ name           = "alka2212/flask-cloud-app:latest"
+ keep_locally   = false
+}
+resource "docker_container" "flask_container" {
+  name  = "flask-app"
+  image = docker_image.flask_app.name
+  ports {
+       internal = 7000
+       external = 7000
+  }
+}
